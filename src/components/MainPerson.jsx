5431988be22f9d0d9c55b9eababbd13dc5cce37f
Zachary Kuchar <zacharyk323@live.com>
2023-07-27 23:42:55 -0400

    Initial setup of props

--- src/components/MainPerson.jsx
+++ src/components/MainPerson.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function MainPerson() {
+function MainPerson(props) {
   return (
     <div>
-      
+      <h1>{props.name}</h1>
     </div>
   )
 }
--- src/components/MainShip.jsx
+++ src/components/MainShip.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function MainShip() {
+function MainShip(props) {
   return (
     <div>
-      
+      <h1>{props.name}</h1>
     </div>
   )
 }
