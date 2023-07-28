41979df9a60ed40fad6eb8cac7cbdbc83a2e3de1
Zachary Kuchar <zacharyk323@live.com>
2023-07-28 15:45:23 -0400

    Set up main routs

--- src/App.js
+++ src/App.js
@@ -2,21 +2,28 @@ import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
 import { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 import SpaceShips from "./pages/SpaceShips";
 import People from "./pages/People";
+import NavBar from "./components/Navbar";
+import NoPage from "./pages/NoPage";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import FilmList from "./pages/FilmsList";
 
 function App() {
   return (
     <div className="App">
       <h1>Star wars</h1>
-      <h1>Header</h1>
-      <h2>--Home Link</h2>
-      <h2>--Films link</h2>
-      <h2>--Database link</h2>
-      <People />
-      <h2>--Star ships Link</h2>
-      <SpaceShips />
-      <h1>Footer</h1>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/films" element={<FilmList />} />
+        <Route path="/characters" element={<People />} />
+        <Route path="/spaceships" element={<SpaceShips />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
