70924a08b72a0c9281978d332ecc9676f29b8f7e
Zachary Kuchar <zacharyk323@live.com>
2023-07-27 22:07:50 -0400

    Set up for calling api with pages

--- src/pages/People.jsx
+++ src/pages/People.jsx
@@ -2,14 +2,41 @@ import axios from "axios";
 import React, {useEffect, useState} from 'react'
 
 function People() {
+    const [people,setPeople] = useState([])
+    const [nextPage, setNextPage] = useState(null)
+    const [prevPage, setPrevPage] = useState(null)
+
     const getData = async () => {
     let data = await axios.get(
-        // "https://swapi.dev/api/people/5"
-        "https://swapi.dev/api/planets/2/"
+        "https://swapi.dev/api/people/"
+        // "https://swapi.dev/api/films/"
+        // "https://swapi.dev/api/planets/2/"
         );
 
     //let item = await data.data;
     console.log(data.data);
+
+    setPeople(data.data.results)
+    setNextPage(data.data.next)
+    setPrevPage(data.data.previous)
+  };
+
+    const loadPage = async (page) => {
+    let data;
+    // for(let i=2; i<=5; i++){
+        data = await axios.get(
+        page
+        );
+
+    //let item = await data.data;
+    //console.log(data.data);
+
+    //items.push(data.data)
+    // }
+    
+    setPeople(data.data.results)
+    setNextPage(data.data.next)
+    setPrevPage(data.data.previous)
   };
 
   useEffect(() => {
@@ -17,7 +44,12 @@ function People() {
   }, []);
   return (
     <div>
-      
+      <h1>People component</h1>
+      {
+        people.map((item)=>{
+            return <h1 key={item.name}>{item.name}</h1>
+        })
+      }
     </div>
   )
 }
--- src/pages/SpaceShips.jsx
+++ src/pages/SpaceShips.jsx
@@ -1,16 +1,48 @@
 import axios from "axios";
 import React, {useEffect, useState} from 'react'
+import DisplayItem from "../components/DisplayItem";
 
 function SpaceShips() {
   const [ships,setShips] = useState([])
+  const [spec,setSpec] = useState([])
+  const [nextPage, setNextPage] = useState(null)
+  const [prevPage, setPrevPage] = useState(null)
 
   const getData = async () => {
-    let data = await axios.get(
-        "https://swapi.dev/api/starships/5"
+    let items=[]
+    let data;
+    // for(let i=2; i<=5; i++){
+        data = await axios.get(
+        `https://swapi.dev/api/starships/`
         );
 
     //let item = await data.data;
     console.log(data.data);
+
+    //items.push(data.data)
+    // }
+    
+    setShips(data.data.results)
+    setNextPage(data.data.next)
+    setPrevPage(data.data.previous)
+  };
+
+  const loadPage = async (page) => {
+    let data;
+    // for(let i=2; i<=5; i++){
+        data = await axios.get(
+        page
+        );
+
+    //let item = await data.data;
+    //console.log(data.data);
+
+    //items.push(data.data)
+    // }
+    
+    setShips(data.data.results)
+    setNextPage(data.data.next)
+    setPrevPage(data.data.previous)
   };
 
   useEffect(() => {
@@ -20,6 +52,13 @@ function SpaceShips() {
   return (
     <div>
       <h1>The SpaceShips Section</h1>
+      {
+        ships.map((item)=>{
+            return <DisplayItem key={item.name} 
+            name={item.name} model={item.model}
+            />
+        })
+      }
     </div>
   )
 }
