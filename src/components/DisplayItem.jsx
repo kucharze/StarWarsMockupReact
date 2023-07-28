5c220f76c87ac5617d3b4f87ec1db0c683c2ad56
Zachary Kuchar <zacharyk323@live.com>
2023-07-27 22:08:10 -0400

    Added props to display element

--- src/components/DisplayItem.jsx
+++ src/components/DisplayItem.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-function DisplayItem() {
+function DisplayItem(props) {
   return (
     <div>
       <h1>The display item</h1>
+      <h3>{props.name}</h3>
+      <h3>{props.model}</h3>
     </div>
   )
 }
