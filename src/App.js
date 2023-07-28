af0aa383376f4c3f22885dc7a9d1c99e545d5db8
Zachary Kuchar <zacharyk323@live.com>
2023-07-27 22:51:26 -0400

    Imported peop

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import axios from "axios";
 import { useEffect } from "react";
 import SpaceShips from "./pages/SpaceShips";
+import People from "./pages/People";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <h2>--Home Link</h2>
       <h2>--Films link</h2>
       <h2>--Database link</h2>
+      <People />
       <h2>--Star ships Link</h2>
       <SpaceShips />
       <h1>Footer</h1>
