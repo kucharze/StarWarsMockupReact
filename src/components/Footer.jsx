ede3fe6a08a6bf89737eeafa1ff6fd7bac79f3bc
Zachary Kuchar <zacharyk323@live.com>
2023-07-28 15:48:37 -0400

    Added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div id='footerWrapper'>
+      <h1> footer here </h1>
+      <p><a href='https://swapi.dev/'>SWAPI, Star Wars API</a></p>
+      <p>TM & © Lucasfilm Ltd. All Rights Reserved</p>
+    </div>
+  )
+}
+
+export default Footer
