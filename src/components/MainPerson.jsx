4d82d9e86044218b41dc8a70ba0e8a50a2fa03d6
Zachary Kuchar <zacharyk323@live.com>
2023-07-28 10:09:41 -0400

    Adjusted styles and elements

--- src/components/MainPerson.jsx
+++ src/components/MainPerson.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 function MainPerson(props) {
   return (
-    <div>
-        <h1>Main person</h1>
+    <div className='MainPerson'>
       <h1>{props.name}</h1>
+      <h2>Birthyear: {props.birth}</h2>
+      <h2>Height: {props.height} meters</h2>
+      <h2>Gender: {props.gender}</h2>
     </div>
   )
 }
