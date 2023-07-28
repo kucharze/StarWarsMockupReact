11900740117b5ecb495b93ab90379e6be948cd3d
Zachary Kuchar <zacharyk323@live.com>
2023-07-28 15:45:41 -0400

    Added modes folder items

---
+++ src/models/buttonArrow.js
@@ -0,0 +1,6 @@
+const arrow = {
+  point:
+    "https://static-mh.content.disney.io/matterhorn/assets/starwars/shared/arrow_button-7be878062232.svg",
+};
+
+module.exports = arrow;
---
+++ src/models/homeImgs.js
@@ -0,0 +1,11 @@
+const landing = [
+  "https://lumiere-a.akamaihd.net/v1/images/sw-outlaws-sdcc-2023-sw2-hero-desktop_5612e467.jpeg?region=0,0,1600,686",
+  "https://lumiere-a.akamaihd.net/v1/images/sdcc-2023-ahsoka-props-sashsingle-hero-desktop_ef762575.jpeg?region=0,0,2048,878",
+  "https://lumiere-a.akamaihd.net/v1/images/sdcc-2023-hasbro-sashsingle-hero-desktop_9ca64590.jpeg?region=0,0,2048,878",
+  "https://lumiere-a.akamaihd.net/v1/images/sdcc-2023-publishing-sashsingle-hero-desktop_13297631.jpeg?region=0,0,2048,878",
+  // ashoka logo
+  // logo: "https://lumiere-a.akamaihd.net/v1/images/ahsoka-logo-white_7d1d4ae5.png?region=0,0,800,263",
+  "https://lumiere-a.akamaihd.net/v1/images/ahsoka-trailer-0711-hero-static-desktop_1b32c6e4.jpeg?region=0,0,2048,878",
+  "https://lumiere-a.akamaihd.net/v1/images/ahsoka-featurette-0710-hero-desktop_5ab03520.jpeg?region=0,0,2048,878",
+];
+module.exports = landing;
