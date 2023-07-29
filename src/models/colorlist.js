77bc5b1657a0de727a042f194c4243a1785fbfa2
Zachary Kuchar <zacharyk323@live.com>
2023-07-29 16:48:44 -0400

    Added images for coloring

---
+++ src/models/colorlist.js
@@ -0,0 +1,23 @@
+const colorList = [
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6NvY_YoTp75H3V2xA6wkotR7mulfQlmn96g&usqp=CAU",
+  "https://printablefreecoloring.com/drawing/movies/coloring-star-wars-70543.jpg",
+  "https://www.supercoloring.com/sites/default/files/styles/coloring_thumbnail/public/cif/2023/02/chibi-yoda-coloring-page.png",
+  "https://www.nicepng.com/png/detail/64-646652_star-wars-coloring-pages-star-wars-colouring-book.png",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRb13oKjFVaOqmQi1Y-C5YtPbDJsrC76P6a-Q&usqp=CAU",
+  "https://playinglearning.com/wp-content/uploads/star-wars-coloring-page-35.png",
+  "https://easydrawingguides.com/wp-content/uploads/2022/12/0674-easy-r2d2-from-star-wars-coloring-page.png",
+  "https://i0.wp.com/kitchenoverlord.com/wp-content/uploads/2015/12/star-wars-coloring-pages-7.gif?resize=654%2C955&ssl=1",
+  "https://www.momjunction.com/wp-content/uploads/2014/08/Top-25-Star-Wars-Coloring-Sheets-Your-Toddler-Will-Love-To-Do-910x1024.jpg",
+  "https://i.pinimg.com/1200x/e1/7a/14/e17a141f1c4da4d8b23bea04e0f9cc24.jpg",
+  "https://wonder-day.com/wp-content/uploads/2020/05/wonder-day-star-wars-2.jpg",
+  "https://www.supercoloring.com/sites/default/files/styles/coloring_medium/public/cif/2015/12/kylo-ren-coloring-page.png",
+  "https://verbnow.com/wp-content/uploads/2021/06/StarWars-Coloring-Illustration-20-page-0-791x1024.jpg.webp",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNN_4UUsdTYEbhmYJvPVK8EK7SQQdYIO6E-A&usqp=CAU",
+  "https://www.cartonionline.com/disegni-da-colorare/Boba_Fett/Boba_Fett_05.jpg",
+  "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a32ed7a3-cb5e-4968-86ab-c9a7faab50e9/dg0oeye-bd7c1e75-d991-40ca-9fb7-a8a8d84d1ecc.jpg/v1/fill/w_774,h_1032,q_70,strp/star_wars_coloring_pages___8__by_coloringpageswk_dg0oeye-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTIwMCIsInBhdGgiOiJcL2ZcL2EzMmVkN2EzLWNiNWUtNDk2OC04NmFiLWM5YTdmYWFiNTBlOVwvZGcwb2V5ZS1iZDdjMWU3NS1kOTkxLTQwY2EtOWZiNy1hOGE4ZDg0ZDFlY2MuanBnIiwid2lkdGgiOiI8PTkwMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.db4WZBA1rjKmZ1Iefgiy47LShQEqg-Yy3Pa34wHXfFQ",
+  "https://mycoloring-pages.com/images/boys/lego-star-wars/coloring-pages-lego-star-wars-10.jpg",
+  "https://www.supercoloring.com/sites/default/files/styles/coloring_full/public/cif/2008/12/star-wars-5-coloring-page.jpg",
+  "https://www.z31.net/images10/star-wars-coloring-pages10.jpg",
+];
+
+module.exports = colorList;
