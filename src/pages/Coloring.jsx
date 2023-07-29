6a6e1606812fc6cfdec806f5c710e3660f6a5304
Zachary Kuchar <zacharyk323@live.com>
2023-07-29 16:53:59 -0400

    Imported coloring list

--- src/pages/Coloring.jsx
+++ src/pages/Coloring.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import MainLayout from '../components/MainLayout'
 import Color from '../components/Color'
+import colorList from '../models/colorlist'
 
 function Coloring() {
   return (
     <MainLayout>
         <div>
-            Hello
+            <h2>Coloring pages</h2>
             <div className='Coloringlist'>
-                <Color/>
+                {
+                    colorList.map((item,i)=>{
+                        return <Color color={item} key={i}/>
+                    })
+                }
+                
             </div>
         </div>
     </MainLayout>
