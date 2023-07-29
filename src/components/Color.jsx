5d4ecb3de872b488211fb1a6b884954b0bc97c61
Zachary Kuchar <zacharyk323@live.com>
2023-07-29 16:54:16 -0400

    Adjusted styling

--- src/components/Color.jsx
+++ src/components/Color.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 function Color(props) {
   return (
     <div>
-      Color item
-      <img src={props.color} alt="" />
+      <img src={props.color} alt="coloringPic" className='Coloritem'/>
     </div>
   )
 }
