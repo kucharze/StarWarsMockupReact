602ca205047b7a233857c1748268069948c17fae
Zachary Kuchar <zacharyk323@live.com>
2023-07-30 14:14:56 -0400

    Adjusted home page and changed styles

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-function HomePage(props) {
+function HomePage({images}) {
+
+  const [currentPic, setCurrentPic] = useState(0)
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      nextImg();
+    }, 8000)
+  })
+
+  // functionality
+  const nextImg = () => {
+    if(currentPic === images.length - 1){
+      setCurrentPic(0)
+    }else{
+      setCurrentPic(currentPic + 1);
+    }
+  }
+
+  const prevImg = () => {
+    if(currentPic === 0){
+      setCurrentPic(images.length - 1)
+    }else{
+      setCurrentPic(currentPic - 1);
+    }
+  }
+
+  //console.log(currentPic)
+
   return (
-    <div>
-      {/* <h1>THIS IS THE HOME PAGE</h1> */}
-      <img src={props.img} alt="images" className='imgSize'/>
+    // <div id='carouselAttempt'>
+    //   {/* <h1>THIS IS THE HOME PAGE</h1> */}
+    //   <img src={props.img[props.count]} alt="images" className='imgSize'/>
+    // </div>
+
+    // carousel 2 attempt
+    <div className='carousel'>
+      {/* CAROUSEL 2 ATTEMPT */}
+      <div className='carouselWrapper'>
+      {images.map((card, i) =>{
+        return (
+          <div key={i} className={i === currentPic ? 'card cardActive' : 'card'}>
+            <img src={"../../public/images/darth.jpeg"} alt="characters" className='carouselImg'/>
+            <div className='cardTextBox'>
+              <h2 className='cardTitle'>{card.name}</h2>
+            </div>
+          </div>
+        );
+      })}
+      <div className="arrowLeft" onClick={prevImg}>&lsaquo;</div>
+      <div className="arrowRight" onClick={nextImg}>&lsaquo;</div>
+      </div>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,51 +1,20 @@
-import React, {  useState} from 'react'
+import React from 'react'
 import MainLayout from '../components/MainLayout'
 import HomePage from '../components/HomePage'
-import homeImgs from '../models/homeImgs'
-import Button from '../components/Button'
+// carousel attempt 2
+import starwars from '../models/carouselData'
 
 // console.log(homeImgs)
 
 function Home() {
-  // pics
-  const [currentPic, setCurrentPic] = useState(homeImgs[0]);
-  // count
-  const [count, setCount] = useState(0)
-
-  const nextImg = () => {
-    let newCount = count + 1
-    if(newCount>5){
-      newCount = 0
-    }
-    setCount(newCount)
-    setCurrentPic(homeImgs[newCount])
-  }
-
-  const prevImg = () => {
-    let newCount = count -1
-    if(newCount<0){
-      newCount = 5
-    }
-    setCount(newCount )
-    setCurrentPic(homeImgs[newCount ])
-  }
-
+  // Carousel Attempt
+ // console.log('carousel images',starwars)
 
   return (
     <div>
       <MainLayout>
-      <div id='btnWrapper'>
-        <div className='leftBtn'>
-            <Button changePic={prevImg} />
-        </div>
-        <div className="rightBtn">
-          <Button changePic={nextImg} />
-        </div>
-           
-          </div>
-        <div id='homePage'>
-          <HomePage count={count} img={currentPic}/> 
-        </div>
+      {/* CAROUSEL ATTEMPT */}
+      <HomePage images={starwars}/>
       </MainLayout>
     </div>
   )
