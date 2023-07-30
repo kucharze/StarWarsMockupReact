a3bc65cd08e86ec4e26519309302fb6ef6096c9a
Zachary Kuchar <zacharyk323@live.com>
2023-07-30 14:14:39 -0400

    Added new model

---
+++ src/models/carouselData.js
@@ -0,0 +1,32 @@
+const starwars = [
+  {
+    pic: "../../public/images/darth.jpeg",
+    name: "Darth Vader",
+  },
+  {
+    pic: "/images/han.jpeg",
+    name: "Han Solo",
+  },
+  {
+    pic: "/images/leia.jpeg",
+    name: "Princess Leia",
+  },
+  {
+    pic: "../images/luke.jpeg",
+    name: "Luke Skywalker",
+  },
+  {
+    pic: "/images/Maul.jpeg",
+    name: "Darth Maul",
+  },
+  {
+    pic: "/images/obi-wan.jpeg",
+    name: "Obi-Wan Kenobi",
+  },
+  {
+    pic: "/images/Yoda.jpeg",
+    name: "Yoda",
+  },
+];
+
+module.exports = starwars;
